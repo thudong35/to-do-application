b4b343588b48997246969cf1e39a555d018b77ee
thanhthu2411 <thu24@byui.edu>
2026-09-10 16:25:51 -0600

    App update

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,30 +1,28 @@
-import { useState, useEffect } from 'react'
-import TaskList from './components/TaskList'
-import './App.css'
-
-
+import { useState, useEffect } from "react";
+import TaskList from "./components/TaskList";
+import "./App.css";
 
 function App() {
-  
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasks')) ||[])
-
+  const [tasks, setTasks] = useState(
+    () => JSON.parse(localStorage.getItem("tasks")) || [],
+  );
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-  }, [tasks])
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
-  const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState("");
 
   function addTask() {
-    if(!inputValue.trim()) return
+    if (!inputValue.trim()) return;
 
-    const newTask = {id: crypto.randomUUID(), name: inputValue, done:false}
-    setTasks([...tasks, newTask])
-    setInputValue("")
+    const newTask = { id: crypto.randomUUID(), name: inputValue, done: false };
+    setTasks([...tasks, newTask]);
+    setInputValue("");
   }
 
   function keyDown(e) {
-    if(e.key === 'Enter') {
-      addTask()
+    if (e.key === "Enter") {
+      addTask();
     }
   }
 
@@ -35,14 +33,21 @@ function App() {
   return (
     <>
       <div>
-          <h1>To-do List App</h1>
-          <input className='task-input' type='text'  value={inputValue} onKeyDown={keyDown}  onChange={(e) => setInputValue(e.target.value)}/>
-          <button className='add-task-btn'  onClick={addTask}>Add New Task</button>
-          <TaskList tasks={tasks} onRemoveTask={removeTask}/>
-          
+        <h1>To-do List App</h1>
+        <input
+          className="task-input"
+          type="text"
+          value={inputValue}
+          onKeyDown={keyDown}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button className="add-task-btn" onClick={addTask}>
+          Add New Task
+        </button>
+        <TaskList tasks={tasks} onRemoveTask={removeTask} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
